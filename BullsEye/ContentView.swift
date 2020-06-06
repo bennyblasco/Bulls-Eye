d9efc6d4e93984b29809c1a0a874c71d72c72d80
Benjamin Inemugha <bennyblasco@gmail.com>
2020-06-06 20:08:19 +0100

    New code
    First Commit

---
+++ BullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BullsEye
+//
+//  Created by Benjamin Inemugha on 04/06/2020.
+//  Copyright © 2020 Techelopers. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
