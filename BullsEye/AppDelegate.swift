6afa7b7129511c852b9fd81bf44810d2ad3e9888
Benjamin Inemugha <bennyblasco@gmail.com>
2020-06-06 20:22:23 +0100

    Small Change

--- BullsEye/AppDelegate.swift
+++ BullsEye/AppDelegate.swift
@@ -13,7 +13,7 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+//Small change
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
