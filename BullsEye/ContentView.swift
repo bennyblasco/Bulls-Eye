bdb63b78d7bd97045cd81c64af7cb6a599ffcbe4
Benjamin Inemugha <bennyblasco@gmail.com>
2020-06-11 22:22:09 +0100

    Added the game layout

--- BullsEye/ContentView.swift
+++ BullsEye/ContentView.swift
@@ -9,13 +9,70 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, Benjamin!")
+    
+    @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
+    
+    var body: some View{
+        VStack{
+            Spacer()
+            //Target row
+            HStack {
+                Text("Slide as close as you can to:")
+                Text("100")
+                    
+            }
+            Spacer()
+            //Slider row
+            
+            HStack{
+                Text("1")
+                Slider(value: self.$sliderValue, in: 1...100)
+                Text("100")
+            }
+            Spacer()
+            
+            //Button row
+            Button(action:{
+                print("Button pressed!")
+                self.alertIsVisible = true
+            }){
+                Text("Hit me!")
+            }
+                Spacer()
+            
+            .alert(isPresented: $alertIsVisible) { () -> Alert in
+                return Alert(title: Text("Hello there!"), message: Text("Slider value is \(self.sliderValue)"), dismissButton: .default(Text("Awesome!")))
+            
+                }
+            Spacer()
+            //Score row
+            HStack{
+                Button(action: {}) {
+                    Text("Start Over")
+                }
+                Spacer()
+                Text("Score:")
+                Text("999999")
+                Spacer()
+                Text("Round:")
+                Text("999")
+                Spacer()
+                Button(action: {}) {
+                    Text("Info")
+                }
+            }
+            .padding(.bottom, 20)
+            
+        }
     }
 }
 
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewLayout(.fixed(width: 896, height: 414))
     }
 }
