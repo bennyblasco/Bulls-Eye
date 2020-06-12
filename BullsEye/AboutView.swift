07f1e61071a6aebf6aa7d1b36ae681d639aad8cc
Benjamin Inemugha <bennyblasco@gmail.com>
2020-06-13 00:05:56 +0100

    Styled the About View and added app icon

--- BullsEye/AboutView.swift
+++ BullsEye/AboutView.swift
@@ -12,13 +12,20 @@ struct AboutView: View {
     var body: some View {
         VStack{
         Spacer()
-        Text("🎯Bulls Eye 🎯")
+            Text("🎯Bulls Eye 🎯\n")
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
         Text("Bulls Eye is a game where you win points by being precise with sliding to the random number")
+        .foregroundColor(Color.white)
         Text("Hit to check your score")
+        .foregroundColor(Color.white)
         Text("The more precise your are, the more score you get")
+        .foregroundColor(Color.white)
         Text("Enjoy!")
+        .foregroundColor(Color.white)
         Spacer()
         Text("Created Benjamin Inemugha")
+        .foregroundColor(Color.white)
         }
         .navigationBarTitle("About Bulls Eye")
         .background(Image("Background"), alignment: .center)
